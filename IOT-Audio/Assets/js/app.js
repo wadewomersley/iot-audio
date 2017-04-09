87e291e9669bab1139e608e47614f3eba92e5241
Wade Womersley <wade85@gmail.com>
2017-04-09 16:54:50 +0100

    CSS improvements + debug for testing styles/JS.

---
+++ IOT-Audio/Assets/js/app.js
@@ -0,0 +1,89 @@
+(function(){    
+    var playlistRowTemplate = '<div class="row"><div class="col-sm-9 fileContainer"></div><div class="col-sm-offset-0 col-sm-3 text-right optionsContainer"></div></div>';
+    var apiKey = location.search.substring(1);
+
+    function sendChange(path, data) {
+        data.apiKey = apiKey;
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/' + path,
+            data: JSON.stringify(data)
+        });
+    }
+
+    function getData(path) {
+        path = path + (path.indexOf('?') > -1 ? '&' : '?') + 'apiKey=' + encodeURIComponent(apiKey);
+        return $.ajax({
+            type: 'get',
+            url: '/api/' + path
+        });
+    }
+
+    function playLink(e) {
+        e.preventDefault();
+
+        sendChange('play', { FileName: decodeURIComponent(this.pathname.substring(1)) });
+    };
+
+    function changeStartupFile(e) {
+        e.preventDefault();
+
+        var inputs = $('input[type="checkbox"].startupFile');
+        var filename = this.value;
+
+        for (var i = 0; i < inputs.length; i++) {
+            if (this != inputs[i]) {
+                inputs[i].checked = false;
+            }
+        }
+
+        sendChange('startupFile', { filename: this.checked ? filename : null });
+    };
+
+    $(document).ready(function () {
+        var $playlist = $('#playlist');
+        var $volume = $('#volume');
+
+        $.when(getData('settings'), getData('playlist'))
+            .then(function (settings, playlist) {
+                settings = settings[0];
+                playlist = playlist[0];
+
+                if (settings.Volume !== null) {
+                    $volume.val(settings.Volume);
+                }
+
+                var files = playlist.Files;
+
+                for (var i = 0; i < files.length; i++) {
+                    var $row = $(playlistRowTemplate);
+                    var $fileContainer = $row.find('.fileContainer');
+                    var $optionsContainer = $row.find('.optionsContainer');
+                    
+                    var $link = $('<a/>');
+                    var $input = $('<input type="checkbox" class="startupFile" />');
+                    var $label = $('<label class="float-right"></label>');
+                    
+                    $input.val(files[i].FileName);
+
+                    $link.attr('href', files[i].FileName);
+                    $link.text(files[i].DisplayName);
+
+                    $input.attr('checked', files[i].FileName === settings.StartupFilename);
+
+                    $input.appendTo($label);
+                    $link.appendTo($fileContainer);
+                    $label.appendTo($optionsContainer);
+                    $row.appendTo($playlist);
+
+                    $link.on('click', playLink);
+                    $input.on('change', changeStartupFile);
+                }
+            });
+
+        $volume.on('change', function (e) {
+            sendChange('volume', { Volume: parseInt($(this).val()) });
+        });;
+    });
+})();
---
+++ IOT-Audio/Assets/js/debug.js
@@ -0,0 +1,26 @@
+// Used for local testing
+if (location.search.indexOf('DEBUG') > -1) {
+    $.ajax = function(args) {
+        console.log('AJAX', args);
+        return new Promise((resolve, reject) => {
+            switch (args.type) {
+                case 'get': 
+                    if (args.url.indexOf('/settings') > -1) {
+                        resolve([{Volume: 75}]);
+                    } else if (args.url.indexOf('/playlist')) {
+                        resolve([{Files: [
+                            {
+                                FileName: "File One.mp3",
+                                DisplayName: "File One"
+                            },
+                            {
+                                FileName: "Another File.mp3",
+                                DisplayName: "Another File"
+                            }
+                        ]}])
+                    }
+                    break;
+            }
+        });
+    };
+}
